51a222910085b6c0cab89c79c404b4b238bc8b8b
Rustin Dodd <rustin.dodd@q2ebanking.com>
2020-04-15 16:41:04 -0500

    service worker fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
   entry: './src/client/index.js',
@@ -19,12 +20,15 @@ module.exports = {
     ]
   },
   output: {
-    filename: 'app.js'
+    filename: 'app.js',
+    libraryTarget: 'var',
+    library: 'Client'
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
-    })
+    }),
+    new WorkboxPlugin.GenerateSW()
   ]
 }
